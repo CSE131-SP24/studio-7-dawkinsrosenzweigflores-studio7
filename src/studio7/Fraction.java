60476012999ce571fcedd7e3d91e5747f253e0bc
jdawkins0704 <jdawkins0704@localhost>
2024-03-27 14:17:08 -0500

    commit

--- src/studio7/Fraction.java
+++ src/studio7/Fraction.java
@@ -6,13 +6,32 @@ public class Fraction {
 	private double denominator; 
 	private double sum; 
 	private double product; 
-	private double reciprocal; 
+	private String reciprocal; 
 	private double simplification; 
 	
+	
+	public Fraction(double numerator, double denominator) {
+		this.numerator = numerator;
+		this.denominator = denominator; 
+	}
+	
+	public String makeString() {
+		Double.toString(numerator);
+		Double.toString(denominator);
+		return 
+		
+	}
+	
+	public void setReciprocal(String reciprocal) {
+		this.reciprocal = "denominator/numerator";
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 	}
 
-}
+	
+	}
+
+
